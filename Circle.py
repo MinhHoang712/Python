25141e9691ae823b948c111460e8411d0513afb1
MinhHoang712 <89459205+MinhHoang712@users.noreply.github.com>
2021-12-03 21:17:27 +0700

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,9 @@
+import turtle
+t=turtle.Turtle()
+t.pensize(5)
+t.pencolor("red")
+t.fillcolor("red")
+t.begin_fill()
+t.circle(60)
+t.end_fill()
+
---
+++ Rec.py
@@ -0,0 +1,10 @@
+import turtle
+rec=turtle.Turtle()
+rec.color("orange")
+for i in range(2):
+    rec.forward(100)
+    rec.right(90)
+    rec.forward(200)
+    rec.right(90)
+rec.exitonclick()
+
