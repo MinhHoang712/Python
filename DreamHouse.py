843c7cc87341ae39978bd641fa906c0b6dbc3315
MinhHoang712 <89459205+MinhHoang712@users.noreply.github.com>
2021-12-06 21:33:09 +0700

    Add files via upload

--- DreamHouse.py
+++ DreamHouse.py
@@ -10,33 +10,34 @@ def picture():
     #mặt trc
     t.fillcolor("blue")
     t.begin_fill()
+    a=100 #độ dài tường
     for i in range(5):
-        t.fd(100)
+        t.fd(a)
         t.rt(90)
     t.end_fill()
     #mặt bên
     t.fillcolor("yellow")
     t.begin_fill()
-    t.left(100)
+    t.left(a)
     t.fd(130)
     t.right(100)
-    t.fd(100)
+    t.fd(a)
     t.right(80)
     t.fd(130)
     t.end_fill()
 
     t.rt(100)
-    t.fd(100)
+    t.fd(a)
     #mái trc
     t.fillcolor("pink")
     t.begin_fill()
     t.left(30)
-    t.fd(100)
+    t.fd(a)
     t.left(120)
-    t.fd(100)
+    t.fd(a)
     t.end_fill()
 
-    t.fd(-100)
+    t.fd(-a)
     t.left(180)
     #mái bên
     t.fillcolor("orange")
@@ -44,20 +45,21 @@ def picture():
     t.right(50)
     t.fd(130)
     t.right(70)
-    t.fd(100)
+    t.fd(a)
     t.right(110)
     t.fd(130)
     t.end_fill()
     t.left(80)
-    t.fd(100)
+    t.fd(a)
     # cửa
     t.right(90)
     t.fd(25)
     t.right(90)
     t.fillcolor("green")
     t.begin_fill()
+    b=a/2 #độ dài cửa
     for i in range(3):
-        t.fd(50)
+        t.fd(b)
         t.left(90)
     t.end_fill()
 
@@ -67,14 +69,15 @@ def picture():
     #cửa sổ
     t.fillcolor("brown")
     t.begin_fill()
+    c=b/2 # độ dài cửa sổ
     t.left(10)
-    t.fd(25)
+    t.fd(c)
     t.right(100)
-    t.fd(25)
+    t.fd(c)
     t.right(80)
-    t.fd(25)
+    t.fd(c)
     t.right(100)
-    t.fd(25)
+    t.fd(c)
     t.end_fill()
     
 
@@ -141,7 +144,7 @@ def picture():
     t.begin_fill()
     t.circle(50)
     t.end_fill()
-    t.exitonclick()
+    turtle.done()
     
 if __name__ == "__main__":
     picture()
