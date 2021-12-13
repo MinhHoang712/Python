1c327f9b746d160d8ae0de039e1e0a2737ffa6c7
MinhHoang712 <89459205+MinhHoang712@users.noreply.github.com>
2021-12-13 21:13:48 +0700

    Create đơn hàng.py

---
+++ đơn hàng.py
@@ -0,0 +1,12 @@
+def orderPrice():
+    num = int(input("Give me a price: "))
+    if num >= 150:
+        print("Total: ", num - 50)
+    elif num >= 100:
+        print("Total: ", num - 25)
+    elif num >= 75:
+        print("Total: ", num - 15)
+    else:
+        print("Total: ", num)
+if __name__ == "__main__":
+    orderPrice()
